15f6fa19f2f085cc783a1c0764e01dc67bd799f8
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-03-19 14:17:46 +0100

    eerste commit

---
+++ src/main/java/be/vdab/web/ControllersConfig.java
@@ -0,0 +1,21 @@
+package be.vdab.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class ControllersConfig implements WebMvcConfigurer {
+	@Bean
+	InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/JSP/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
