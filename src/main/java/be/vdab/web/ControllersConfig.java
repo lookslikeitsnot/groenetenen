a9135dba9c07afaea326f81de480ade24167d053
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-03-19 15:29:15 +0100

    first working online

--- src/main/java/be/vdab/web/ControllersConfig.java
+++ src/main/java/be/vdab/web/ControllersConfig.java
@@ -1,9 +1,27 @@
 package be.vdab.web;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -17,5 +35,96 @@ public class ControllersConfig implements WebMvcConfigurer {
 		resolver.setPrefix("/WEB-INF/JSP/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+		registry.addResourceHandler("/styles/**").addResourceLocations("/styles/");
+		registry.addResourceHandler("/scripts/**").addResourceLocations("/scripts/");
+	}
+
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addFormatters(FormatterRegistry registry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Validator getValidator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public MessageCodesResolver getMessageCodesResolver() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
