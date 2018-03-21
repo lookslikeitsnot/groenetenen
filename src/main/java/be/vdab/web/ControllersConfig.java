073f5785ae30231a20b09f88cd8ad2ecd5df7e57
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-03-21 08:36:17 +0100

    met parameters in postmapping

--- src/main/java/be/vdab/web/ControllersConfig.java
+++ src/main/java/be/vdab/web/ControllersConfig.java
@@ -37,92 +37,109 @@ public class ControllersConfig implements WebMvcConfigurer {
 		return resolver;
 	}
 
+	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
 		registry.addResourceHandler("/styles/**").addResourceLocations("/styles/");
 		registry.addResourceHandler("/scripts/**").addResourceLocations("/scripts/");
 	}
 
+	@Override
 	public void configurePathMatch(PathMatchConfigurer configurer) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void addFormatters(FormatterRegistry registry) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		// TODO Auto-generated method stub
-		
+		registry.addViewController("/info").setViewName("info");
 	}
 
+	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public Validator getValidator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public MessageCodesResolver getMessageCodesResolver() {
 		// TODO Auto-generated method stub
 		return null;
--- src/main/java/be/vdab/web/FiliaalController.java
+++ src/main/java/be/vdab/web/FiliaalController.java
@@ -1,7 +1,10 @@
 package be.vdab.web;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 class FiliaalController {
 	private static final String FILIALEN_VIEW = "filialen/filialen";
 	private static final String TOEVOEGEN_VIEW = "filialen/toevoegen";
+	private static final String REDIRECT_URL_NA_TOEVOEGEN = "redirect:/filialen";
+	private static final Logger LOGGER = Logger.getLogger(FiliaalController.class.getName());
 
 	@GetMapping
 	String findAll() {
@@ -18,5 +23,11 @@ class FiliaalController {
 	@GetMapping("toevoegen")
 	String createForm() {
 		return TOEVOEGEN_VIEW;
+	}
+
+	@PostMapping
+	String create() {
+		LOGGER.info("filiaal record toevoegen aan database");
+		return REDIRECT_URL_NA_TOEVOEGEN;
 	}
 }
